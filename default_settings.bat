38b01074f3962c2c113bbe9579ff37a7500ec3f6
Matthew Tarchick <mtarchick13@gmail.com>
2021-04-01 10:49:11 -0400

    added a batch file

---
+++ default_settings.bat
@@ -0,0 +1,11 @@
+@echo off
+cls
+:start
+cd /d c:/pyscripts/wavelet_analysis
+python wavelet_analysis.py
+set choice=
+set /p choice="Do you want to restart? Press 'y' and enter for Yes: "
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='y' goto start
+
+
