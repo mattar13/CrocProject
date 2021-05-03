07c383bceb190b43b5a6f51ce4d20e1c7735b4a7
Matthew Tarchick <mtarchick13@gmail.com>
2021-05-03 14:48:38 -0400

    no need to edit system paths

--- default_settings.bat
+++ default_settings.bat
@@ -1,9 +1,9 @@
 @echo off
 cls
-call activate.bat 
+call %UserProfile%/Anaconda3/Scripts/activate.bat 
 :start
 cd /d %cd%/src/
-python wavelet_analysis.py
+%UserProfile%/Anaconda3/python wavelet_analysis.py
 set choice=
 set /p choice="Do you want to restart? Press 'y' and enter for Yes: "
 if not '%choice%'=='' set choice=%choice:~0,1%
